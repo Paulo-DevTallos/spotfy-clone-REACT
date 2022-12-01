0da3f1f5413e806bc08f8ee0b5b9c3d6f513a944
Paulo Sergio <pauloserg861@gmail.com>
2022-12-01 11:23:46 -0300

    implementando return

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,19 +2,34 @@ import { HeaderHome } from "../../components/HeaderHome";
 import { Banner, ContainerImg, Content } from './style'
 
 import bannerImg from '../../assets/img/banner-home.jpg'
+import { Button } from "../../components/Button";
 
-export const Home = () => (
-	<>
-		<HeaderHome />
-		<main>
-			<Banner>
-			<Content>
-				ola
-			</Content>
-			<ContainerImg>
-				<img src={bannerImg} alt="banner da home" />
-			</ContainerImg>
-			</Banner>
-		</main>
-	</>
-);
+export const Home = () => {
+	return(
+		<>
+			<HeaderHome />
+			<main>
+				<Banner>
+					<Content>
+						<h1>R$ 0,00 por 3 meses de Premium</h1>
+						<p>Começa hoje a ouvir música sem anúncios e offline grátis até 1 de março de 2023. Cancela quando quiseres.</p>
+						<div>
+							<Button>
+								Experimente por 3 meses por R$ 0,00
+							</Button>
+							<Button>
+								Ver Planos
+							</Button>
+						</div>
+						<small>
+						Somente no plano Individual. Depois, é só R$ 19,90/mês. Sujeito a Termos e Condições. Disponível apenas para quem nunca usou o Premium. A oferta termina em 31/12/2022.
+						</small>
+					</Content>
+					<ContainerImg>
+						<img src={bannerImg} alt="banner da home" />
+					</ContainerImg>
+				</Banner>
+			</main>
+		</>
+	)
+};
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -4,11 +4,30 @@ export const Banner = styled.section`
 	padding: 20px 90px 0;
 	display: flex;
 	background-color: #df0c8b;
+	color: #fff;
 	height: 430px;
 `
 
 export const Content = styled.div`
 	width: 65%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+
+	h1 {
+		font-size: 2.5rem;
+		letter-spacing: -1px;
+		font-weight: 900;
+		line-height: 1.2;
+		font-family: 'Circular Spotify Tx T Black';
+	}
+
+	p {
+		line-height: 1.5;
+		font-weight: 400;
+		font-size: 1.4rem;
+		margin: 30px 0;
+	}
 `
 
 export const ContainerImg = styled.div`
