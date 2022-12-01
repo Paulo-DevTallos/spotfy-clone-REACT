252087493badf052afd8ebb3adb7cf0e97010f00
Paulo Sergio <pauloserg861@gmail.com>
2022-12-01 11:36:23 -0300

    instalando bibliotecas de estilos

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,5 +1,19 @@
-export const Button = ({ children }) => (
-	<button>
-		{ children }
-	</button>
-)
+import { ContainerBtn } from "./style"
+
+import { createUseStyles } from "react-jss"
+
+const useStyles = createUseStyles({
+	bgButton: {
+		backgroundColor: 'red',
+	}
+})
+
+export const Button = ({ children }) => {
+	const classes = useStyles()
+
+	return(
+		<ContainerBtn className={classes.bgButton}>
+			{ children }
+		</ContainerBtn>
+	)
+}
---
+++ src/components/Button/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ContainerBtn = styled.button`
+	color: #fff;
+	text-align: center;
+	padding: 12px 25px;
+	border-radius: 25px;
+	text-transform: uppercase;
+	font-family: 'Circular Spotify Tx T Black', Arial, Helvetica, sans-serif;
+	margin: 3px;
+`
