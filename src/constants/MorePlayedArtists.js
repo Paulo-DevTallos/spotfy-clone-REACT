b7361df6b36ee8ea0f323f1f7f665874edb75d39
Paulo Sergio <pauloserg861@gmail.com>
2022-12-01 15:53:16 -0300

    criando seção de mais tocados

---
+++ src/constants/MorePlayedArtists.js
@@ -0,0 +1,31 @@
+import ImageOne from '../assets/img/artists/image-1.jpg'
+import ImageTwo from '../assets/img/artists/image-2.jpg'
+import ImageThree from '../assets/img/artists/image-3.jpg'
+import ImageFour from '../assets/img/artists/image-4.jpg'
+
+export const MorePlayed = [
+	{
+		image: ImageOne,
+		title: 'O Album',
+		subtitle: 'tocado',
+		artist: 'Manifesto Musical Henrique & Juliano',
+	},
+	{
+		image: ImageTwo,
+		title: 'O Artista',
+		subtitle: 'tocado',
+		artist: 'Marilia Mendonça',
+	},
+	{
+		image: ImageThree,
+		title: 'O Podcast',
+		subtitle: 'tocado',
+		artist: 'Podpah',
+	},
+	{
+		image: ImageFour,
+		title: 'A Música',
+		subtitle: 'tocada',
+		artist: 'Mal Feito - Ao Vivo Hugo & Guilherme',
+	},
+]
